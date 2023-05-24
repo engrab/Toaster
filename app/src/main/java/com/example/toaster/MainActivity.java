07b91bf6576aeac1365fa4644609c346b997a8c2
engrab <engrbilawalhussain@gmail.com>
2023-05-24 15:12:01 +0500

    Initial commit

---
+++ app/src/main/java/com/example/toaster/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.toaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toaster.showToast(this, "this is demo toasting library");
+    }
+}
